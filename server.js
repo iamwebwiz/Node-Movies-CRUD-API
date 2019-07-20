14aeaaf7b7fad636275b809dacb03e0da3252004
Ezekiel <iamwebwiz@gmail.com>
2019-07-20 23:42:12 +0100

    Add server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const server = express();
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
