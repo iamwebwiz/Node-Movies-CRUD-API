3bef847f67f7ccd88780dc6db0ebbbceeb009197
Ezekiel <iamwebwiz@gmail.com>
2019-07-21 00:34:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./router");
-const port = process.env.port || 3000;
+require("dotenv").config();
+const port = process.env.PORT || 3000;
 
 const server = express();
 
 // Middlewares
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
+
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,PATCH,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 server.use("/api/v1", router);
 
 server.listen(port, () => console.log(`Server listening at port ${port}`));
