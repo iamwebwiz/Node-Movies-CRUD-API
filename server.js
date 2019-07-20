c4cc6e97823a0a3ebd5de25de618194b4bcd53d6
Ezekiel <iamwebwiz@gmail.com>
2019-07-20 23:50:23 +0100

    Add router.js

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const router = require("./router");
+const port = process.env.port || 3000;
 
 const server = express();
 
+// Middlewares
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
+server.use("/api/v1", router);
+
+server.listen(port, () => console.log(`Server listening at port ${port}`));
